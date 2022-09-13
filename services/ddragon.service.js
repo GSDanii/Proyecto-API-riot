1398aecaca715cbaa17b38fa6f025bd939d87323
Your Name <you@example.com>
2022-09-13 16:13:53 +0200

    ddragon functionalities, print images

--- services/ddragon.service.js
+++ services/ddragon.service.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const getSixItems = require("../utils/getRandomKeys")
+const getOneChamp = require("../utils/getChampions")
 
 class DDragonService {
     constructor() {
@@ -6,10 +8,23 @@ class DDragonService {
             baseURL: 'http://ddragon.leagueoflegends.com/cdn'
         });
     }
-    getAllItems() {
-        return this.axios.get('/12.17.1/data/es_ES/item.json').then((res) => res.data)
+    getItemKeys() {
+        return this.axios.get('/12.17.1/data/es_ES/item.json').then((res) => {
+            const itemsObj = res.data.data
+            let keys = Object.keys(itemsObj)
+            return getSixItems(keys)
+        })
     }
 
+
+    getChampion() {
+        return this.axios.get('/12.17.1/data/es_ES/champion.json').then((res) => {
+            const champions = res.data.data
+            let keys = Object.keys(champions)
+            return getOneChamp(keys)
+
+        })
+    }
 }
 
 module.exports = new DDragonService()
